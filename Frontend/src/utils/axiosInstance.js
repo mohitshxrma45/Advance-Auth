d6ed9a2f94d6d26a6061f9ef20f1c3656856c717
Mohit <iammohitpandit52@gmail.com>
2026-06-13 22:05:44 +0530

    Deployment

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3000/api/auth",
+    baseURL: "https://advance-auth-tx5p.onrender.com/api/auth",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
