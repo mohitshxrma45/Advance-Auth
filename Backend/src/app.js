6627315027e818d709bc31a5054402ee7eac66ef
Mohit <iammohitpandit52@gmail.com>
2026-06-15 16:43:05 +0530

    dp

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -17,6 +17,10 @@ app.use(
         credentials: true,
     })
 );
+app.use((req, res, next) => {
+    console.log(req.method, req.url);
+    next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
