b8615891dff1eddac726e461884970ea1128e00b
Mohit <iammohitpandit52@gmail.com>
2026-06-22 15:57:09 +0530

    ddd

--- Frontend/src/utils/axiosInstance.js
+++ Frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-    baseURL: "https://advance-auth-2-b24m.onrender.com/api/auth",
+    baseURL: "https://advance-auth-4.onrender.com/api/auth",
     withCredentials: true,
     headers: {
         "Content-Type": "application/json",
